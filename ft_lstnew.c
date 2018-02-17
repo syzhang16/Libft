d27c65d825456b117c37500034548d1bea400fe9
sylvie zhang <sylvie@sylvies-MacBook-Pro.local>
2018-02-17 17:09:56 +0100

    ajout iter et new

---
+++ ft_lstnew.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: syzhang <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/04 15:18:24 by syzhang           #+#    #+#             */
+/*   Updated: 2017/12/04 15:18:27 by syzhang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list	*new;
+
+	if (!(new = (t_list*)malloc(sizeof(t_list))))
+		return (NULL);
+	if (!(new->content = malloc(content_size)))
+		return (NULL);
+	if (!content)
+	{
+		new->content = NULL;
+		new->content_size = 0;
+	}
+	else
+	{
+		new->content = ft_memcpy(new->content, content, content_size);
+		new->content_size = content_size;
+	}
+	new->next = NULL;
+	return (new);
+}
